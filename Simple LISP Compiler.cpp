946f42d87819349dfab831779b964d4859f46a90
TheVex <66883116+TheVex@users.noreply.github.com>
2025-09-08 17:09:54 +0300

    Добавьте файлы проекта.

---
+++ Simple LISP Compiler.cpp
@@ -0,0 +1,12 @@
+﻿// Simple LISP Compiler.cpp: определяет точку входа для приложения.
+//
+
+#include "Simple LISP Compiler.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
